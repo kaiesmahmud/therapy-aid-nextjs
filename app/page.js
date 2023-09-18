1841612cf60d2d5a82b9cfe57c45173b2a70f87e
kaiesmahmud <kaiesmahmud@gmail.com>
2023-09-18 21:07:05 +0600

    font -updated

--- app/page.js
+++ app/page.js
@@ -3,9 +3,10 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex gap-10 justify-center items-center">
-      Therapy-Aid
+      
 
-      <p className='montserret-font'>Therapy Aid</p>
+      <p className='roboto-font text-[50px]'>Therapy Aid</p>
+      <p className=' text-[50px]'>Therapy Aid</p>
     </main>
   )
 }
