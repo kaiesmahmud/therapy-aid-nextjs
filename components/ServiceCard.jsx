1f2827b4d6ada8b053937246bc2ffaeb420e73d7
kaiesmahmud <kaiesmahmud@gmail.com>
2023-09-20 13:09:33 +0600

    mb added

--- components/ServiceCard.jsx
+++ components/ServiceCard.jsx
@@ -10,7 +10,7 @@ const ServiceCard = ({details}) => {
             <h3 className="text-lg font-medium md:text-3xl lg:text-4xl mb-3">{details.title}</h3>
             <hr className=" border-blue-800 mb-2"/>
             <h4 className="text-md md:text-lg font-medium ">Purpose:</h4>
-            <p className=" text-slate-800 roboto-font">{details.purpose}</p>
+            <p className=" text-slate-800 roboto-font mb-2">{details.purpose}</p>
             <h4 className="text-md md:text-lg font-medium ">Provide:</h4>
             <p className=" text-slate-800 roboto-font">{details.provided}</p>
         </div>
