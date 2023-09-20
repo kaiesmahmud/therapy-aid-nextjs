5d48e609ecb973d894432db9d5c4b0a0896a15c5
kaiesmahmud <kaiesmahmud@gmail.com>
2023-09-20 11:27:42 +0600

    text updated

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
       <div className=' flex flex-col lg:flex-row flex-wrap p-5 items-center justify-center gap-1 md:gap-2 lg:gap-4  pt-20 lg:pt-52 w-[100%] h-full' >
 
         <div className=' shadow-lg  p-5 lg:py-20 lg:px-10 rounded-xl bg-gradient-to-tr from-cyan-500 to-blue-500 bg-opacity-5 w-full lg:w-[40vw] lg:min-w-[600px]'>
-          <h1 className='sm:text-xl md:text-3xl lg:text-4xl font-light capitalize'>Seeking highly-trained and experienced Allied health Assistants and support staff to help you achieve your goals?</h1>
+          <h1 className='sm:text-xl md:text-3xl lg:text-4xl font-semibold capitalize'>Seeking highly-trained and experienced Allied health Assistants and support staff to help you achieve your goals?</h1>
           <div className='mt-5 py-5 flex flex-col gap-3 opacity-80 text-xs md:text-base '>
             <h3 className='flex items-center  gap-2'><BsCheckCircleFill className='text-green-500'/> Experienced Allied Health  Assistants</h3>
             <h3 className='flex items-center  gap-2'><BsCheckCircleFill className='text-green-500'/>Well-Trained Support Workers</h3>
