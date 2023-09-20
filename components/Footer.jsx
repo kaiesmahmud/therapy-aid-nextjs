0601015f7e0ff95f31674c62905ea31d0db933f7
kaiesmahmud <kaiesmahmud@gmail.com>
2023-09-20 15:11:22 +0600

    updated

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='flex flex-col md:flex-row gap-5 items-center justify-center p-5'>
+      <p>
+      Copyright &copy;2023 All Right Researved | Therapy AID
+      </p>
+      <div className=' font-semibold flex flex-wrap items-center justify-center gap-5'>
+        <p>Home</p>
+        <p>Experience</p>
+        <p>Services</p>
+        <p>Contact</p>
+      </div> 
+    </footer>
   )
 }
 
