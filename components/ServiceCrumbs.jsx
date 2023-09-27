24477a16000765971a5e8c4de1796590179f4e57
kaiesmahmud <kaiesmahmud@gmail.com>
2023-09-27 09:23:43 +0600

    updated

--- components/ServiceCrumbs.jsx
+++ components/ServiceCrumbs.jsx
@@ -30,7 +30,7 @@ const ServiceCrumbs = () => {
     },
     {
       id:'05',
-      title:"LawnMowing..",
+      title:"Transport..",
       link: '/services/#4',
     },
     {
@@ -40,7 +40,7 @@ const ServiceCrumbs = () => {
     },
     {
       id:'07',
-      title:"Transport..",
+      title:"LawnMowing..",
       link: '/services/#6',
     },
   ]
